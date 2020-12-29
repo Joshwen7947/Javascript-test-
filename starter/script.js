844c1fcfbfe7fa060c4ad01b75158d1a69f453ad
Josh Wenner <68701564+Joshwen7947@users.noreply.github.com>
2020-12-29 23:15:34 +0700

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,92 @@
+
+let js = 'amazing';
+
+console.log();
+
+let firstName = 'josh';
+console.log(firstName);
+
+let lastName = 'wenner';
+
+console.log(firstName + ' ' + lastName);
+
+let myFirstJob = 'Produce';
+let myCurrentJob = 'Teacher';
+
+console.log(myCurrentJob);
+
+let country = 'Vietnam';
+let continent = 'Asia';
+let city = 'Ho Chi Minh City';
+
+console.log(country + continent + city);
+
+
+let jsisfun = true;
+console.log(jsisfun);
+
+console.log(typeof true);
+
+jsisfun = 'YES';
+console.log(jsisfun);
+
+var test
+console.log(test);
+
+test = 12;
+console.log(test);
+
+
+let age = 23;
+age = 24;
+
+const birthYear = 1997;
+
+// Old code
+var job = 'Teacher';
+job = 'Programmer';
+* /
+
+const now = 2020;
+
+const ageJosh = now - 1997;
+const ageThao = now - 1998;
+console.log(ageJosh, ageThao);
+
+console.log(ageJosh * 2, ageJosh / 2, 2 ** 3);
+
+// 2** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Josh';
+const lastName = 'Wenner';
+
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x /= 4; // x = x / 4 = 25
+x++;
+console.log(x);
+
+// comparison operators 
+console.log(ageJosh > ageThao);
+console.log(ageThao >= 21);
+
+const isFullAge = ageThao >= 21;
+console.log(now - 1997 > now - 1998);
+
+let isIsland = 'Vietnam';
+let language
+console.log(isIsland, language);
+
+language = 'vietnamese';
+
+let population = 100000000;
+population++;
+console.log(population);
+const finland = 6000000;
+
+console.log(population > finland);
+
+let description = 'Portugal is in Europe, and its 11 million people speak Portugeuse';
